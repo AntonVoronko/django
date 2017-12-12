4ef847022fe83fe007eca987e46476499b9436ce
AntonVoronko <voronko2012@gmail.com>
2017-12-12 19:37:37 +0200

    hello6

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-	data = [ bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]    
+    data = [ bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]    
 	return iter(data)
