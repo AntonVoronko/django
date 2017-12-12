7236158181a6cef8f41d90b9bb7a63cc522d3123
AntonVoronko <voronko2012@gmail.com>
2017-12-12 20:29:54 +0200

    hello2

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    data = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]    
-    return data
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
